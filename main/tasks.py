2cecbe0686af4d9418a98ed19c3b365df36f1cd1
GithubHungry <bardiervadim97@gmail.com>
2020-08-03 16:07:01 +0300

    add_schedule

--- main/tasks.py
+++ main/tasks.py
@@ -1,8 +1,22 @@
 from send_email.celery import app
+from django.core.mail import send_mail
 
+from .models import Contact
 from .service import send
 
 
 @app.task
 def send_spam_email(user_email):
     send(user_email)
+
+
+@app.task
+def send_beat_email():
+    for contact in Contact.objects.all():
+        send_mail(
+            'Вы подписались на рассылку',
+            'Мы будем прписылать вам много информации каждые 5 минут.',
+            'shopmanage7@gmail.com',
+            [contact.email],
+            fail_silently=False,
+        )
--- send_email/celery.py
+++ send_email/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'send_email.settings')  # Указываем где находятся джанговский модуль и файл-settings проекта,
@@ -8,3 +9,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 app = Celery('send_email')  # Создаем объект celery и даем ему имя send_email
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# schedule
+app.conf.beat_schedule = {
+    'send-spam_every-5-minute': {   # task name in schedule
+        'task': 'main.tasks.send_beat_email',   # register task + name of task in tasks.py
+        'schedule': crontab(minute='*/5'),   # переодичность выполнения таски
+    },
+}
