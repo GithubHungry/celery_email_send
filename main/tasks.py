ceccd9546c963cad5aec823c998117433253608c
GithubHungry <bardiervadim97@gmail.com>
2020-08-03 16:35:42 +0300

    add_tasks_and_flower

--- main/tasks.py
+++ main/tasks.py
@@ -1,3 +1,5 @@
+from celery import shared_task
+
 from send_email.celery import app
 from django.core.mail import send_mail
 
@@ -20,3 +22,28 @@ def send_beat_email():
             [contact.email],
             fail_silently=False,
         )
+
+
+@app.task
+def my_task(a, b):
+    c = a + b
+    return c
+
+
+@app.task
+def my_task_as(d, e):
+    c = d + e
+    return c
+
+
+@app.task(bind=True, default_retry_delay=5 * 60)  # Задаем время для автоматического перезапуска рухнувшего таска.
+def my_task_retry(self, x, y):
+    try:
+        return x + y
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=60)  # Задаем время для автоматического перезапуска рухнувшего таска.
+
+
+@shared_task()
+def my_sh_task(msg):
+    return msg + '!!!'
